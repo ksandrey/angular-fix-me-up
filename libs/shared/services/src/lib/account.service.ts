404566ebcafe7048701ff2c1975bd87be78307d9
ksandrey <111602673+ksandrey@users.noreply.github.com>
2022-08-19 21:39:54 -0400

    Update account.service.ts

--- libs/shared/services/src/lib/account.service.ts
+++ libs/shared/services/src/lib/account.service.ts
@@ -1,18 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { of } from 'rxjs';
+import { Subject  } from 'rxjs';
 import { Account } from './account';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  getAccounts(): Observable<Account[]> {
-    const accounts: Account[] = [
-      { id: "1234", balance: 7500, currency: "cad" },
-      { id: "1235", balance: 4500, currency: "cad" },
-      { id: "1236", balance: 2102, currency: "usd" },
-    ];
-    return of(accounts);
+
+  accounts: Account[] = [
+    { id: "1234", balance: 7500, currency: "cad" },
+    { id: "1235", balance: 4500, currency: "cad" },
+    { id: "1236", balance: 2102, currency: "usd" }];
+
+  accounts$: Subject<Account[]> = new Subject();
+  
+  // getAccounts(): Observable<Account[]> {
+  //   const accounts: Account[] = [
+  //     { id: "1234", balance: 7500, currency: "cad" },
+  //     { id: "1235", balance: 4500, currency: "cad" },
+  //     { id: "1236", balance: 2102, currency: "usd" },
+  //   ];
+  //   return of(accounts);
+  // }
+
+  updateList(accountsFilter: string) {
+    //this.accounts = this.accounts.filter(acc => acc.currency === accountsFilter || accountsFilter === '');
+    this.accounts$.next(this.accounts.filter(acc => acc.currency === accountsFilter || accountsFilter === ''));
   }
+  
 }
